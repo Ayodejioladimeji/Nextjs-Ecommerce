4877ffd628e8762d205b0034b192749253966026
Ayodejioladimeji <layobright11@gmail.com>
2022-02-10 17:02:03 -0800

    Implemented the registration page

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
@@ -11,13 +11,17 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+    username: {
+      type: String,
+      required: true,
+    },
     password: {
       type: String,
       required: true,
     },
     role: {
       type: String,
-      default: "user",
+      default: 'user',
     },
     root: {
       type: Boolean,
@@ -26,7 +30,7 @@ const userSchema = new mongoose.Schema(
     avatar: {
       type: String,
       default:
-        "https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png",
+        'https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png',
     },
   },
   {
@@ -34,5 +38,5 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-let Dataset = mongoose.models.user || mongoose.model("user", userSchema);
+let Dataset = mongoose.models.user || mongoose.model('user', userSchema);
 export default Dataset;
