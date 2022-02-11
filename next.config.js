8b552e3c2f2a0eba54f641d00b5ac741abee4c4f
Ayodejioladimeji <layobright11@gmail.com>
2022-02-11 09:25:25 -0800

    added new config file

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    BASE_URL: 'http://localhost:3000',
-    MONGODB_URL:
-      'mongodb+srv://layobright:Basiratu1@cluster0.2gntf.mongodb.net/nextjs?retryWrites=true&w=majority',
-  },
-};
